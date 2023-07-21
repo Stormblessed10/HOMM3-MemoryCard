e7151351476973bf2cfe5de64f9ccf3672982322
Stormblessed <carlomanid@gmail.com>
2023-07-22 00:11:20 +0300

    Change props to context

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,8 @@
-export function Header({ score, level, best, isMenuOpen }) {
+import { useCards } from "../context/CardsContext";
+
+export function Header() {
+  const { score, level, best, isMenuOpen } = useCards();
+
   return <header>
     <h1>Memory Game</h1>
     {!isMenuOpen && <div className="stats">
