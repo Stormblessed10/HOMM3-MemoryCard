c85b4cb016d34b1d5e939a36fa86ae57a39b7406
Stormblessed <carlomanid@gmail.com>
2023-07-21 23:57:29 +0300

    Add functions

--- src/context/CardsContext.js
+++ src/context/CardsContext.js
@@ -15,25 +15,6 @@ const initialState = {
     isMenuOpen: true,
 }
 
-function shuffle(arr) {
-    const newArr = arr;
-    for (let i = newArr.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      const temp = newArr[j];
-      newArr[j] = newArr[i];
-      newArr[i] = temp;
-    }
-    return newArr;
-  }
-
-function uniqueCards(cards, level) {
-    const uniqueIndexes = new Set();
-    while (uniqueIndexes.size !== 4 * level) {
-        uniqueIndexes.add(Math.floor(Math.random() * cards.length));
-    }
-    return Array.from(uniqueIndexes, (i) => cards[i]);
-}
-
 function reducer(state, action) {
     switch(action.type) {
         case "loaded":
@@ -62,21 +43,21 @@ const WIN_SCORE = 6;
 export function CardsProvider({ children }) {
     const [{level, allCards, levelCards, clicked, score, best, fact, error, isLoading, isMenuOpen}, dispatch] = useReducer(reducer, initialState);
     
-      useEffect(() => {
-        async function getCards() {
-            dispatch({type: "loaded"});
+    useEffect(() => {
+    async function getCards() {
+        dispatch({type: "loaded"});
 
-            try {
-                const res = await fetch(URL_CARDS);
-                const data = await res.json();
-                dispatch({type: "fetchCards", payload: data});
-            } catch(err) {
-                dispatch({type: "rejected", payload: err});
-            }
+        try {
+            const res = await fetch(URL_CARDS);
+            const data = await res.json();
+            dispatch({type: "fetchCards", payload: data});
+        } catch(err) {
+            dispatch({type: "rejected", payload: err});
         }
-      }, []);
+    }
+    }, []);
 
-      async function getFact() {
+    async function getFact() {
         dispatch({type: "loaded"});
         
         try {
@@ -89,9 +70,28 @@ export function CardsProvider({ children }) {
         } catch(err) {
             dispatch({type: "rejected", payload: err});
         }
-      }
+    }
 
-    return <CardsContext.Provider value={{level, allCards, levelCards, clicked, score, best, fact, error, isLoading, isMenuOpen, winScore: WIN_SCORE, getFact, dispatch}}>{children}</CardsContext.Provider>
+    function shuffle(arr) {
+        const newArr = arr;
+        for (let i = newArr.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            const temp = newArr[j];
+            newArr[j] = newArr[i];
+            newArr[i] = temp;
+        }
+        return newArr;
+    }
+
+    function uniqueCards() {
+        const uniqueIndexes = new Set();
+        while (uniqueIndexes.size !== 4 * level) {
+            uniqueIndexes.add(Math.floor(Math.random() * allCards.length));
+        }
+        return Array.from(uniqueIndexes, (i) => allCards[i]);
+    }
+
+    return <CardsContext.Provider value={{level, allCards, levelCards, clicked, score, best, fact, error, isLoading, isMenuOpen, winScore: WIN_SCORE, getFact, dispatch, uniqueCards, shuffle}}>{children}</CardsContext.Provider>
 }
 
 export function useCards() {
