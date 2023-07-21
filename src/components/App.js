90614457878e06731e3c50b6fea27ccf5be7de4f
Stormblessed <carlomanid@gmail.com>
2023-07-22 00:09:15 +0300

    Change JSX

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,14 @@
-import { useEffect, useState } from "react";
-import { cards } from "../data";
 import { CardList } from "./CardList";
 import { Header } from "./Header";
 import { Main } from "./Main";
+import { CardsProvider } from "../context/CardsContext";
 
 export default function App() {
- 
 
-  return 
-  // <div className={score === winScore ? "app light-theme" : "app"}>
-  //   <Header best={best} level={level} score={score} isMenuOpen={isMenuOpen}/>
-  //   <Main winScore={winScore} best={best} score={score} isMenuOpen={isMenuOpen} onStartGame={handleStartGame}>
-  //     <blockquote className="message">
-  //     {isLoading ? <span className="loading">Loading...</span> : <span>{message}</span> }
-  //     </blockquote>
-  //     <CardList collection={collection} onHover={() => 1} onCardClick={handleCardClick}/>
-  //   </Main>
-  // </div>
+  return <CardsProvider>
+     <div>
+       <Header/>
+       <Main/>
+     </div>
+  </CardsProvider>
 }
