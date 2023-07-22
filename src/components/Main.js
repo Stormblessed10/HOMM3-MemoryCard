8df0902aa38ead713a44c6c08e67fc73183d65f3
Stormblessed <carlomanid@gmail.com>
2023-07-22 17:38:15 +0300

    Add spinner

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,17 @@
 import { useCards } from "../context/CardsContext";
 import { CardList } from "./CardList";
 import Menu from "./Menu";
+import Spinner from "./Spinner";
+import styles from "./Main.module.css";
 
 export function Main() {
-  const {isMenuOpen} = useCards();
+  const {isMenuOpen, isLoading} = useCards();
 
-  if (isMenuOpen) return <main><Menu/></main>
-
-  return <main>
-    <blockquote className="message"></blockquote>
+  return <main className={styles.main}>
+    {isMenuOpen && <Menu/>}
+    {!isMenuOpen && (!isLoading ? <>
+    <blockquote className={styles.message}>Hello</blockquote>
     <CardList/>
+    </> : <Spinner/>)}
   </main>;
 }
