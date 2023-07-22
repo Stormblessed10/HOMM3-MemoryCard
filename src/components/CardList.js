ff9b19ffcfe9d3236a15e450c26e41278fb8b49c
Stormblessed <carlomanid@gmail.com>
2023-07-22 19:56:11 +0300

    Add styles

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,10 +1,11 @@
 import { useCards } from "../context/CardsContext";
 import { CardItem } from "./CardItem";
+import styles from "./CardList.module.css";
 
 export function CardList() {
   const { levelCards } = useCards();
 
-  return <ul className="cards">
-    {levelCards.map(card => <CardItem key={card.name} card={card} />)}
+  return <ul className={styles.cards}>
+    {levelCards.map((card) => <CardItem key={card.name} card={card} />)}
   </ul>;
 }
